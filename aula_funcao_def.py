29ecf82ba658d6e9b0b260bbe4a95247e524e0f4
PARFEL <125777351+PARFEL@users.noreply.github.com>
2023-06-06 17:26:42 -0300

    save

--- aula_funcao_def.py
+++ aula_funcao_def.py
@@ -3,6 +3,10 @@ import random
 def soma(a, b ,c):
     print(f'{a=} + {b=} + {c=} = ', a + b + c)
 
-soma(15, 90, 10)
-soma(10, 10, 10)
-soma(b=10, c=10, a=20)
+num1 = random.randint(0, 500)
+num2 = random.randint(0, 600)
+num3 = random.randint(0, 700)
+
+soma(num1, num2, num3)
+soma(num1, num2, num3)
+soma(b=num1, c=num2, a=num3)
